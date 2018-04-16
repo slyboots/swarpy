bdf25897a3de5827cfdc7821c5469146205cd943
Dakota Wisdom Lorance <dwl@dakotalorance.com>
2018-04-15 19:26:32 -0500

    updated project structure and added click for cli

--- swarpy/profiles.py
+++ swarpy/profiles.py
@@ -1,10 +1,13 @@
 #! python3
 '''send requests to the /profiles endpoint of the swarfarm api'''
-
+import os
 import json
+import click
 import requests
 
-SFAPI_URL = 'https://swarfarm.com/api/v2/profiles/'
+SFAPI_URL = os.getenv('SFAPI_URL')
+
+
 def get_profile(user_id):
   '''Get the profile information'''
   res = requests.get(f"{SFAPI_URL}{user_id}/")
